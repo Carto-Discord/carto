0dccb585662bb4a65cf3c81dbf4ca4c61cf0f754
Miles Bardon <milesbardon@gmail.com>
2021-04-18 13:10:01 +0100

    Changing invoker to be all authenticated users

--- deployment/iam.tf
+++ deployment/iam.tf
@@ -1,10 +1,8 @@
-data "google_app_engine_default_service_account" "default" {}
-
 resource "google_cloudfunctions_function_iam_member" "invoker" {
   project        = google_cloudfunctions_function.carto_api.project
   region         = google_cloudfunctions_function.carto_api.region
   cloud_function = google_cloudfunctions_function.carto_api.name
 
   role   = "roles/cloudfunctions.invoker"
-  member = "serviceAccount:${data.google_app_engine_default_service_account.default.email}"
+  member = "allAuthenticatedUsers"
 }
