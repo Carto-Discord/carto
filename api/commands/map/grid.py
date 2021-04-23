95c7b1fdeb77c00b20014aef493272e88b53c350
Miles Bardon <milesbardon@gmail.com>
2021-04-23 23:32:19 +0100

    Updating grid label rendering

--- api/commands/map/grid.py
+++ api/commands/map/grid.py
@@ -40,51 +40,64 @@ def column_string(n):
     return string
 
 
+def find_font_size(text, max_width, max_height):
+    font_height = 1
+
+    try:
+        font = ImageFont.truetype("arial.ttf", font_height)
+        while font.getsize(text)[0] < max_width and font.getsize(text)[1] < max_height:
+            font_height += 1
+            font = ImageFont.truetype("arial.ttf", font_height)
+    except OSError as oe:
+        logging.log(level=logging.WARN, msg=oe)
+        font = ImageFont.load_default()
+
+    return font
+
+
 def apply_grid(image_url: str, rows: int, cols: int):
     image_name = download_image(image_url)
     if image_name == '':
         return None
 
     file_name = 'map.png'
-    margin_top = 20
-    margin_left = 10 + math.floor(math.log(rows, 10)) * 10
-    font_height = 12
     line_colour = (0xff, 0xff, 0xff)
 
     with Image.open(image_name) as im:
+        col_width = im.size[0] / cols
+        row_height = im.size[1] / rows
+
+        margin_left = int(col_width)
+        margin_top = int(row_height)
+
         frame = Image.new('RGB', tuple(map(operator.add, im.size, (margin_left, margin_top))), (0xff, 0xff, 0xff))
 
         map_draw = ImageDraw.Draw(im)
         frame_draw = ImageDraw.Draw(frame)
 
-        try:
-            font = ImageFont.truetype("arial.ttf", font_height)
-        except OSError:
-            font = ImageFont.load_default()
-
-        col_width = im.size[0] / cols
-        row_height = im.size[1] / rows
-
         reverse_row_values = range(rows, 0, -1)
+        row_font = find_font_size(str(reverse_row_values[0]), int(margin_left * 0.8), int(margin_top * 0.8))
+        col_font = find_font_size(column_string(cols), int(margin_left * 0.8), int(margin_top * 0.8))
+
         for i in range(0, rows):
             y = (i + 1) * row_height
             map_draw.line([0, y, im.size[0], y], line_colour)
 
             label = str(reverse_row_values[i])
-            w, h = font.getsize(label)
+            w, h = row_font.getsize(label)
             y_label = y - (row_height / 2) - (h / 2)
-            frame_draw.text((margin_left / 2 - w / 2, y_label), label, font=font, fill=0)
+            frame_draw.text((margin_left / 2 - w / 2, y_label), label, font=row_font, fill=0)
 
         for j in range(0, cols):
             x = (j + 1) * col_width
             map_draw.line([x, 0, x, im.size[1]], line_colour)
 
             label = column_string(j + 1)
-            w, h = font.getsize(label)
-            x_label = x - (col_width / 2) + margin_top - (w / 2)
-            frame_draw.text((x_label, im.size[1] + margin_top / 2 - h / 2), label, font=font, fill=0)
+            w, h = col_font.getsize(label)
+            x_label = x - (col_width / 2) - (w / 2) + margin_left
+            frame_draw.text((x_label, im.size[1] + margin_left / 2 - h / 2), label, font=col_font, fill=0)
 
-        frame.paste(im, (margin_left, 0))
+        frame.paste(im, (int(col_width), 0))
 
         if os.getenv('IS_TEST', 'false') == 'false':
             file_name = '/tmp/' + file_name
