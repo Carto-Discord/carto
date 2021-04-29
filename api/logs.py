1a25be62289344f6088361b3fea3085756fd6398
Miles Bardon <milesbardon@gmail.com>
2021-04-29 22:02:59 +0100

    Adding execution ID to logs

--- api/logs.py
+++ api/logs.py
@@ -4,18 +4,22 @@ from google.cloud.logging import Client, Resource
 class Logger(object):
     res = None
     logger = None
+    execution_id = None
 
     @staticmethod
     def setup(execution_id):
         log_client = Client()
         log_name = 'cloudfunctions.googleapis.com%2Fcloud-functions'
+        Logger.execution_id = execution_id
         Logger.res = Resource(type="cloud_function",
                               labels={
                                   "function_name": "carto-api",
-                                  "execution_id": execution_id
                               })
         Logger.logger = log_client.logger(log_name)
 
     @staticmethod
     def log(message, severity='INFO'):
-        Logger.logger.log_struct({"message": message}, resource=Logger.res, severity=severity)
+        Logger.logger.log_struct({"message": message},
+                                 resource=Logger.res,
+                                 severity=severity,
+                                 labels={'execution_id': Logger.execution_id})
