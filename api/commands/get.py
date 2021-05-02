a6e3d9f4070cc951d7315eb5d44deb2d4231cef9
Miles Bardon <milesbardon@gmail.com>
2021-05-02 22:26:00 +0100

    Sending text representation of the token with a GET request

--- api/commands/get.py
+++ api/commands/get.py
@@ -14,7 +14,16 @@ def get_channel_map(request_params):
 
     if uuid is None:
         abort(make_response(jsonify(message="This channel has no current map associated"), 404))
-    else:
-        return (jsonify(blob="{}.png".format(uuid), bucket=constants.BUCKET),
-                200,
-                {'Content-Type': 'application/json'})
+
+    channel_map_data = database.get_map_info(uuid)
+    message = ''
+
+    if 'tokens' in channel_map_data:
+        tokens = channel_map_data['tokens']
+        message = 'Tokens on map:\n'
+        for token in tokens:
+            message += f"{token['name']}: ({token['column']}, {token['row']})\n"
+
+    return (jsonify(blob="{}.png".format(uuid), bucket=constants.BUCKET, message=message),
+            200,
+            {'Content-Type': 'application/json'})
--- api/tests/commands/test_get.py
+++ api/tests/commands/test_get.py
@@ -13,15 +13,45 @@ class GetTest(unittest.TestCase):
     app = Flask(__name__)
 
     @patch('commands.map.database.get_current_channel_map')
+    @patch('commands.map.database.get_map_info')
     @patch('commands.constants.BUCKET', 'bucket')
-    def test_get_channel_map(self, mock_get_map, mock_log):
+    def test_get_channel_map(self, mock_map_info, mock_get_map, mock_log):
         mock_get_map.return_value = '1234'
+        mock_map_info.return_value = {
+            'tokens': [
+                {
+                    'name': 'token1',
+                    'row': 4,
+                    'column': 'AA'
+                },
+                {
+                    'name': 'token2',
+                    'row': 7,
+                    'column': 'B'
+                }
+            ]
+        }
 
         with self.app.app_context():
             response = get_channel_map({'channelId': '4567'})
 
         self.assertEqual(json.loads(response[0].data)['blob'], '1234.png')
         self.assertEqual(json.loads(response[0].data)['bucket'], 'bucket')
+        self.assertEqual(json.loads(response[0].data)['message'], 'Tokens on map:\ntoken1: (AA, 4)\ntoken2: (B, 7)\n')
+
+    @patch('commands.map.database.get_current_channel_map')
+    @patch('commands.map.database.get_map_info')
+    @patch('commands.constants.BUCKET', 'bucket')
+    def test_get_channel_map_no_tokens(self, mock_map_info, mock_get_map, mock_log):
+        mock_get_map.return_value = '1234'
+        mock_map_info.return_value = {}
+
+        with self.app.app_context():
+            response = get_channel_map({'channelId': '4567'})
+
+        self.assertEqual(json.loads(response[0].data)['blob'], '1234.png')
+        self.assertEqual(json.loads(response[0].data)['bucket'], 'bucket')
+        self.assertEqual(json.loads(response[0].data)['message'], '')
 
     @patch('commands.map.database.get_current_channel_map')
     def test_get_channel_map_no_uuid(self, mock_get_map, mock_log):
