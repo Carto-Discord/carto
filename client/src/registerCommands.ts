6deac6558704cb9336039c4fae8009e0f1eba389
Miles Bardon <milesbardon@gmail.com>
2021-05-03 18:16:10 +0100

    Adding Slash Commands definition and registration step

---
+++ client/src/registerCommands.ts
@@ -0,0 +1,168 @@
+import {
+  ApplicationCommandOptionType,
+  DiscordInteractions,
+  PartialApplicationCommand,
+} from "slash-commands";
+
+const command: PartialApplicationCommand = {
+  name: "carto",
+  description: "Create and update maps within this channel",
+  options: [
+    {
+      name: "map",
+      description: "Map commands",
+      type: ApplicationCommandOptionType.SUB_COMMAND_GROUP,
+      options: [
+        {
+          name: "create",
+          description: "Create a new map",
+          type: ApplicationCommandOptionType.SUB_COMMAND,
+          options: [
+            {
+              name: "url",
+              description: "Public URL of the map image",
+              required: true,
+              type: ApplicationCommandOptionType.STRING,
+            },
+            {
+              name: "rows",
+              description: "Number of grid rows",
+              required: true,
+              type: ApplicationCommandOptionType.INTEGER,
+            },
+            {
+              name: "columns",
+              description: "Number of grid columns",
+              required: true,
+              type: ApplicationCommandOptionType.INTEGER,
+            },
+          ],
+        },
+        {
+          name: "get",
+          description: "Get the map in its current state",
+          type: ApplicationCommandOptionType.SUB_COMMAND,
+        },
+      ],
+    },
+    {
+      name: "token",
+      description: "Create or update tokens on an existing map",
+      type: ApplicationCommandOptionType.SUB_COMMAND_GROUP,
+      options: [
+        {
+          name: "add",
+          description: "Add a token to the map",
+          type: ApplicationCommandOptionType.SUB_COMMAND,
+          options: [
+            {
+              name: "name",
+              description: "Unique name for this token on the map",
+              required: true,
+              type: ApplicationCommandOptionType.STRING,
+            },
+            {
+              name: "row",
+              description: "The starting row for this token",
+              required: true,
+              type: ApplicationCommandOptionType.INTEGER,
+            },
+            {
+              name: "column",
+              description: "The starting column from this token",
+              required: true,
+              type: ApplicationCommandOptionType.STRING,
+            },
+            {
+              name: "colour",
+              description: "The token colour. Default: Random",
+              required: false,
+              type: ApplicationCommandOptionType.STRING,
+            },
+            {
+              name: "size",
+              description: "Token size",
+              type: ApplicationCommandOptionType.STRING,
+              required: false,
+              choices: [
+                {
+                  name: "Tiny",
+                  value: "TINY",
+                },
+                {
+                  name: "Small",
+                  value: "SMALL",
+                },
+                {
+                  name: "Medium",
+                  value: "MEDIUM",
+                },
+                {
+                  name: "Large",
+                  value: "LARGE",
+                },
+                {
+                  name: "Huge",
+                  value: "HUGE",
+                },
+                {
+                  name: "Gargantuan",
+                  value: "GARGANTUAN",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          name: "move",
+          description: "Move a token on the map",
+          type: ApplicationCommandOptionType.SUB_COMMAND,
+          options: [
+            {
+              name: "name",
+              description: "Name of the token to move",
+              required: true,
+              type: ApplicationCommandOptionType.STRING,
+            },
+            {
+              name: "row",
+              description: "Row to move the token to",
+              required: true,
+              type: ApplicationCommandOptionType.INTEGER,
+            },
+            {
+              name: "column",
+              description: "Column to move the token to",
+              required: true,
+              type: ApplicationCommandOptionType.STRING,
+            },
+          ],
+        },
+        {
+          name: "delete",
+          description: "Remove a token from the map",
+          type: ApplicationCommandOptionType.SUB_COMMAND,
+          options: [
+            {
+              name: "name",
+              description: "Name of the token to delete",
+              required: true,
+              type: ApplicationCommandOptionType.STRING,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+const interaction = new DiscordInteractions({
+  applicationId: process.env.APPLICATION_ID,
+  authToken: process.env.BOT_TOKEN,
+  publicKey: process.env.PUBLIC_KEY,
+});
+
+interaction
+  .createApplicationCommand(command)
+  .then(console.log)
+  .catch(console.error);
