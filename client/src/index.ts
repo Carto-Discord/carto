2c928c3dc762be4233d740201f4c2441234db873
Miles Bardon <milesbardon@gmail.com>
2021-05-03 21:17:43 +0100

    Refactoring index as serverless function

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,208 +1,168 @@
-import Discord from "discord.js";
-import express from "express";
-import path from "path";
-import dotenv from "dotenv";
-import { createMap } from "./create";
+import Discord, { WebhookClient } from "discord.js";
+import {
+  ApplicationCommandInteractionDataOption,
+  InteractionResponseType,
+  InteractionType,
+  Interaction,
+} from "slash-commands";
+import { HttpFunction } from "@google-cloud/functions-framework/build/src/functions";
+import { createMap, CreateProps } from "./create";
 import { getMap } from "./get";
-import { deleteChannel } from "./delete";
-import { addToken, deleteToken, moveToken } from "./token";
+import {
+  AddProps,
+  addToken,
+  DeleteProps,
+  deleteToken,
+  MoveProps,
+  moveToken,
+} from "./token";
+import { validateRequest } from "./utils/validation";
+import { CommandGroup, SubCommand, CommandOptions } from "./types";
 
-dotenv.config();
-
-// Front end routing - BEGIN
-const app = express();
-
-app.get("/", (_req, res) => {
-  res.set("X-Clacks-Overhead", "GNU Terry Pratchet");
-  res.sendFile(path.join(__dirname + "/index.html"));
-});
-
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`);
-});
-// Front end routing - END
-
-// Server Discord client - BEGIN
-const client = new Discord.Client();
-const prefix = "!";
-
-client.once("ready", () => {
-  console.log("Ready!");
-});
-
-const getParameters = (content: string) => {
-  const spaceIndex = content.indexOf(" ");
-  return spaceIndex >= 0 ? content.substr(spaceIndex + 1).split(" ") : [];
+const extractParameters = <T extends CommandOptions>(
+  command: ApplicationCommandInteractionDataOption
+) => {
+  if ("options" in command) {
+    return command.options.reduce((acc, o) => {
+      if ("value" in o) acc[o.name] = o.value;
+      return acc;
+    }, {} as T);
+  }
 };
 
-client.on("message", async (message) => {
-  if (message.content.startsWith(`${prefix}create`)) {
-    console.log(`Received creation request: ${message.content}`);
-    const parameters = getParameters(message.content);
+const handleMapCommands = async (
+  command: ApplicationCommandInteractionDataOption,
+  channelId: string,
+  client: WebhookClient
+) => {
+  // We can be confident that each subcommand will have the correct parameters,
+  // as this is type checked by Discord before reaching here.
+  switch (command.name) {
+    case SubCommand.MAP_CREATE:
+      console.log("Received creation request");
+      const { url, rows, columns } = extractParameters<CreateProps>(command);
 
-    const [url, rows, columns] = parameters;
+      let response = await createMap({
+        url,
+        rows,
+        columns,
+        channelId,
+      });
 
-    if (!url || !rows || !columns) {
-      message.channel.send("Create usage: `!create <url> <rows> <columns>`");
-      return;
-    }
-
-    if (isNaN(+rows) || isNaN(+columns)) {
-      message.channel.send("Rows and Columns must be numbers");
-      return;
-    }
-
-    const response = await createMap({
-      url,
-      rows: Number(rows),
-      columns: Number(columns),
-      channelId: message.channel.id,
-    });
-
-    response.success
-      ? message.channel.send(`Map created for ${message.author.toString()}`, {
-          files: [response.body],
-        })
-      : message.channel.send(response.body);
-  }
-
-  if (message.content.startsWith(`${prefix}token add`)) {
-    console.log(`Received token update request: ${message.content}`);
-    const parameters = getParameters(message.content);
-
-    const [_, name, row, column, colour, size, condition] = parameters;
-
-    if (!name || !row || !column) {
-      message.channel.send(
-        "Add token usage: `!token add <name> <row> <column> <colour> <size>`"
-      );
-      return;
-    }
-
-    if (isNaN(+row)) {
-      message.channel.send("Row must be a number");
-      return;
-    }
-
-    const response = await addToken({
-      name,
-      row: Number(row),
-      column,
-      size: size?.toUpperCase(),
-      condition,
-      colour,
-      channelId: message.channel.id,
-    });
-
-    response.success
-      ? message.channel.send(
-          `Token ${name} added by ${message.author.toString()}`,
-          {
-            files: [response.body],
-          }
-        )
-      : message.channel.send(response.body);
-  }
-
-  if (message.content.startsWith(`${prefix}token move`)) {
-    console.log(`Received token move request: ${message.content}`);
-    const parameters = getParameters(message.content);
-
-    const [_, name, row, column] = parameters;
-
-    if (!name || !row || !column) {
-      message.channel.send(
-        "Move token usage: `!token move <name> <row> <column>`"
-      );
-      return;
-    }
-
-    if (isNaN(+row)) {
-      message.channel.send("Row must be a number");
-      return;
-    }
-
-    const response = await moveToken({
-      name,
-      row: Number(row),
-      column,
-      channelId: message.channel.id,
-    });
-
-    response.success
-      ? message.channel.send(
-          `Token ${name} moved by ${message.author.toString()}`,
-          {
-            files: [response.body],
-          }
-        )
-      : message.channel.send(response.body);
-  }
-
-  if (message.content.startsWith(`${prefix}token delete`)) {
-    console.log(`Received token deletion request: ${message.content}`);
-    const parameters = getParameters(message.content);
-
-    const [_, name] = parameters;
-
-    if (!name) {
-      message.channel.send("Delete token usage: `!token delete <name>`");
-      return;
-    }
-
-    const response = await deleteToken({
-      name,
-      channelId: message.channel.id,
-    });
-
-    response.success
-      ? message.channel.send(
-          `Token ${name} deleted by ${message.author.toString()}`,
-          {
-            files: [response.body],
-          }
-        )
-      : message.channel.send(response.body);
-  }
-
-  if (message.content.startsWith(`${prefix}map`)) {
-    console.log(`Received Map get request: ${message.content}`);
-
-    const response = await getMap({
-      channelId: message.channel.id,
-    });
-
-    response.success
-      ? message.channel
-          .send("Map retrieved", {
+      response.success
+        ? client.send(`Map created`, {
             files: [response.body],
           })
-          .then((message) => {
-            response.message && message.channel.send(response.message);
+        : client.send(response.body);
+      break;
+
+    case SubCommand.MAP_GET:
+      console.log("Received Map get request");
+      response = await getMap({ channelId });
+
+      response.success
+        ? client.send(`Map retrieved`, {
+            files: [response.body],
           })
-      : message.channel.send(response.body);
+        : client.send(response.body);
+      break;
+
+    default:
+      break;
+  }
+};
+
+const handleTokenCommands = async (
+  command: ApplicationCommandInteractionDataOption,
+  channelId: string,
+  client: WebhookClient
+) => {
+  // We can be confident that each subcommand will have the correct parameters,
+  // as this is type checked by Discord before reaching here.
+  switch (command.name) {
+    case SubCommand.TOKEN_ADD:
+      console.log("Received token creation request");
+      let { name, row, column, colour, size } = extractParameters<AddProps>(
+        command
+      );
+
+      let response = await addToken({
+        name,
+        row,
+        column,
+        colour,
+        size,
+        channelId,
+      });
+
+      response.success
+        ? client.send(`Token ${name} added`, {
+            files: [response.body],
+          })
+        : client.send(response.body);
+      break;
+
+    case SubCommand.TOKEN_MOVE:
+      console.log("Received token move request");
+      ({ name, row, column } = extractParameters<MoveProps>(command));
+      response = await moveToken({ name, row, column, channelId });
+
+      response.success
+        ? client.send(`Token ${name} moved`, {
+            files: [response.body],
+          })
+        : client.send(response.body);
+      break;
+
+    case SubCommand.TOKEN_DELETE:
+      console.log("Received token deletion request");
+      ({ name } = extractParameters<DeleteProps>(command));
+      response = await deleteToken({ name, channelId });
+
+      response.success
+        ? client.send(`Token ${name} deleted`, {
+            files: [response.body],
+          })
+        : client.send(response.body);
+      break;
+
+    default:
+      break;
+  }
+};
+
+export const slashFunction: HttpFunction = async (req, res) => {
+  validateRequest(req, res);
+
+  if (req.method !== "POST") {
+    res.status(405).end();
+    return;
   }
 
-  if (message.content.startsWith(`${prefix}help`)) {
-    const channel = await message.author.createDM();
+  const body: Interaction = req.body;
 
-    const helpMessage =
-      "How to use Carto - Default values are in brackets:\n" +
-      "Create a new map\n`!create <public url> <rows> <columns>`\n" +
-      "Add a new token to the map\n`!token add <name> <row> <column> <colour (random)> <size (Medium)>`\n" +
-      "Show the current channel's map\n`!map`\n" +
-      "**Important Info**\nMap states will be deleted after 30 days. Changing the map in any way will extend this period.\n" +
-      "Deleting a channel will delete all associated maps. The deleter will recieve the final map state as DM.";
-    await channel.send(helpMessage);
-
-    message.channel.send("Help instructions have been sent to your DMs");
+  if (body.type === InteractionType.PING) {
+    res.status(200).json({ type: InteractionResponseType.PONG }).end();
+    return;
   }
-});
 
-client.on("channelDelete", async (channel) => {
-  await deleteChannel({ channelId: channel.id });
-});
+  const commandGroup = body.data;
+  const { channelId, id, token } = body;
 
-client.login(process.env.BOT_TOKEN);
-// Server Discord client - END
+  const client = new Discord.WebhookClient(id, token);
+
+  switch (commandGroup.name) {
+    case CommandGroup.MAP:
+      await handleMapCommands(commandGroup.options[0], channelId, client);
+      break;
+
+    case CommandGroup.TOKEN:
+      await handleTokenCommands(commandGroup.options[0], channelId, client);
+      break;
+
+    default:
+      break;
+  }
+
+  res.status(200).end();
+};
