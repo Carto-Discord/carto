4205eed78f430573211ee7e9f1c5a950bee1f448
Miles Bardon <milesbardon@gmail.com>
2021-05-06 21:44:35 +0100

    Opening up client to all requests

--- deployment/iam.tf
+++ deployment/iam.tf
@@ -1,8 +1,17 @@
-resource "google_cloudfunctions_function_iam_member" "invoker" {
+resource "google_cloudfunctions_function_iam_member" "api_invoker" {
   project        = google_cloudfunctions_function.carto_api.project
   region         = google_cloudfunctions_function.carto_api.region
   cloud_function = google_cloudfunctions_function.carto_api.name
 
   role   = "roles/cloudfunctions.invoker"
   member = "allAuthenticatedUsers"
+}
+
+resource "google_cloudfunctions_function_iam_member" "client_invoker" {
+  project        = google_cloudfunctions_function.carto_client.project
+  region         = google_cloudfunctions_function.carto_client.region
+  cloud_function = google_cloudfunctions_function.carto_client.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
 }
