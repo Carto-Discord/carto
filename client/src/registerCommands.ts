a44b91e1086224aef46f98a133b059bf6eb13d40
Miles Bardon <milesbardon@gmail.com>
2021-05-07 22:47:22 +0100

    Splitting commands to match conventions

--- client/src/registerCommands.ts
+++ client/src/registerCommands.ts
@@ -5,152 +5,145 @@ import {
 } from "slash-commands";
 import { CommandGroup, SubCommand } from "./types";
 
-const command: PartialApplicationCommand = {
-  name: "carto",
-  description: "Create and update maps within this channel",
+const mapCommand: PartialApplicationCommand = {
+  name: CommandGroup.MAP,
+  description: "Map commands",
   options: [
     {
-      name: CommandGroup.MAP,
-      description: "Map commands",
-      type: ApplicationCommandOptionType.SUB_COMMAND_GROUP,
+      name: SubCommand.MAP_CREATE,
+      description: "Create a new map",
+      type: ApplicationCommandOptionType.SUB_COMMAND,
       options: [
         {
-          name: SubCommand.MAP_CREATE,
-          description: "Create a new map",
-          type: ApplicationCommandOptionType.SUB_COMMAND,
-          options: [
-            {
-              name: "url",
-              description: "Public URL of the map image",
-              required: true,
-              type: ApplicationCommandOptionType.STRING,
-            },
-            {
-              name: "rows",
-              description: "Number of grid rows",
-              required: true,
-              type: ApplicationCommandOptionType.INTEGER,
-            },
-            {
-              name: "columns",
-              description: "Number of grid columns",
-              required: true,
-              type: ApplicationCommandOptionType.INTEGER,
-            },
-          ],
+          name: "url",
+          description: "Public URL of the map image",
+          required: true,
+          type: ApplicationCommandOptionType.STRING,
         },
         {
-          name: SubCommand.MAP_GET,
-          description: "Get the map in its current state",
-          type: ApplicationCommandOptionType.SUB_COMMAND,
+          name: "rows",
+          description: "Number of grid rows",
+          required: true,
+          type: ApplicationCommandOptionType.INTEGER,
+        },
+        {
+          name: "columns",
+          description: "Number of grid columns",
+          required: true,
+          type: ApplicationCommandOptionType.INTEGER,
         },
       ],
     },
     {
-      name: CommandGroup.TOKEN,
-      description: "Create or update tokens on an existing map",
-      type: ApplicationCommandOptionType.SUB_COMMAND_GROUP,
+      name: SubCommand.MAP_GET,
+      description: "Get the map in its current state",
+      type: ApplicationCommandOptionType.SUB_COMMAND,
+    },
+  ],
+};
+
+const tokenCommand: PartialApplicationCommand = {
+  name: CommandGroup.TOKEN,
+  description: "Create or update tokens on an existing map",
+  options: [
+    {
+      name: SubCommand.TOKEN_ADD,
+      description: "Add a token to the map",
+      type: ApplicationCommandOptionType.SUB_COMMAND,
       options: [
         {
-          name: SubCommand.TOKEN_ADD,
-          description: "Add a token to the map",
-          type: ApplicationCommandOptionType.SUB_COMMAND,
-          options: [
+          name: "name",
+          description: "Unique name for this token on the map",
+          required: true,
+          type: ApplicationCommandOptionType.STRING,
+        },
+        {
+          name: "row",
+          description: "The starting row for this token",
+          required: true,
+          type: ApplicationCommandOptionType.INTEGER,
+        },
+        {
+          name: "column",
+          description: "The starting column from this token",
+          required: true,
+          type: ApplicationCommandOptionType.STRING,
+        },
+        {
+          name: "colour",
+          description: "The token colour. Default: Random",
+          required: false,
+          type: ApplicationCommandOptionType.STRING,
+        },
+        {
+          name: "size",
+          description: "Token size",
+          type: ApplicationCommandOptionType.STRING,
+          required: false,
+          choices: [
             {
-              name: "name",
-              description: "Unique name for this token on the map",
-              required: true,
-              type: ApplicationCommandOptionType.STRING,
+              name: "Tiny",
+              value: "TINY",
             },
             {
-              name: "row",
-              description: "The starting row for this token",
-              required: true,
-              type: ApplicationCommandOptionType.INTEGER,
+              name: "Small",
+              value: "SMALL",
             },
             {
-              name: "column",
-              description: "The starting column from this token",
-              required: true,
-              type: ApplicationCommandOptionType.STRING,
+              name: "Medium",
+              value: "MEDIUM",
             },
             {
-              name: "colour",
-              description: "The token colour. Default: Random",
-              required: false,
-              type: ApplicationCommandOptionType.STRING,
+              name: "Large",
+              value: "LARGE",
             },
             {
-              name: "size",
-              description: "Token size",
-              type: ApplicationCommandOptionType.STRING,
-              required: false,
-              choices: [
-                {
-                  name: "Tiny",
-                  value: "TINY",
-                },
-                {
-                  name: "Small",
-                  value: "SMALL",
-                },
-                {
-                  name: "Medium",
-                  value: "MEDIUM",
-                },
-                {
-                  name: "Large",
-                  value: "LARGE",
-                },
-                {
-                  name: "Huge",
-                  value: "HUGE",
-                },
-                {
-                  name: "Gargantuan",
-                  value: "GARGANTUAN",
-                },
-              ],
+              name: "Huge",
+              value: "HUGE",
+            },
+            {
+              name: "Gargantuan",
+              value: "GARGANTUAN",
             },
           ],
         },
+      ],
+    },
+    {
+      name: SubCommand.TOKEN_MOVE,
+      description: "Move a token on the map",
+      type: ApplicationCommandOptionType.SUB_COMMAND,
+      options: [
         {
-          name: SubCommand.TOKEN_MOVE,
-          description: "Move a token on the map",
-          type: ApplicationCommandOptionType.SUB_COMMAND,
-          options: [
-            {
-              name: "name",
-              description: "Name of the token to move",
-              required: true,
-              type: ApplicationCommandOptionType.STRING,
-            },
-            {
-              name: "row",
-              description: "Row to move the token to",
-              required: true,
-              type: ApplicationCommandOptionType.INTEGER,
-            },
-            {
-              name: "column",
-              description: "Column to move the token to",
-              required: true,
-              type: ApplicationCommandOptionType.STRING,
-            },
-          ],
+          name: "name",
+          description: "Name of the token to move",
+          required: true,
+          type: ApplicationCommandOptionType.STRING,
         },
         {
-          name: SubCommand.TOKEN_DELETE,
-          description: "Remove a token from the map",
-          type: ApplicationCommandOptionType.SUB_COMMAND,
-          options: [
-            {
-              name: "name",
-              description: "Name of the token to delete",
-              required: true,
-              type: ApplicationCommandOptionType.STRING,
-            },
-          ],
+          name: "row",
+          description: "Row to move the token to",
+          required: true,
+          type: ApplicationCommandOptionType.INTEGER,
+        },
+        {
+          name: "column",
+          description: "Column to move the token to",
+          required: true,
+          type: ApplicationCommandOptionType.STRING,
+        },
+      ],
+    },
+    {
+      name: SubCommand.TOKEN_DELETE,
+      description: "Remove a token from the map",
+      type: ApplicationCommandOptionType.SUB_COMMAND,
+      options: [
+        {
+          name: "name",
+          description: "Name of the token to delete",
+          required: true,
+          type: ApplicationCommandOptionType.STRING,
         },
       ],
     },
@@ -164,6 +157,11 @@ const interaction = new DiscordInteractions({
 });
 
 interaction
-  .createApplicationCommand(command)
+  .createApplicationCommand(mapCommand)
+  .then(console.log)
+  .catch(console.error);
+
+interaction
+  .createApplicationCommand(tokenCommand)
   .then(console.log)
   .catch(console.error);
