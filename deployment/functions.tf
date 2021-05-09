bd1b35738e5a7b582a60075f1d80eae4dc97143e
Miles Bardon <milesbardon@gmail.com>
2021-05-09 21:08:47 +0100

    Doubling client ram

--- deployment/functions.tf
+++ deployment/functions.tf
@@ -18,7 +18,7 @@ resource "google_cloudfunctions_function" "carto_client" {
   name    = "${var.app_name}-client"
   runtime = "nodejs14"
 
-  available_memory_mb   = 128
+  available_memory_mb   = 256
   source_archive_bucket = google_storage_bucket.code_archives.name
   source_archive_object = google_storage_bucket_object.client_archive.name
   trigger_http          = true
