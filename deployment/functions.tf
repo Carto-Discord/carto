79bd32eb3302a5215d60ef42386c37b947c576d7
Miles Bardon <milesbardon@gmail.com>
2021-05-10 21:58:00 +0100

    Redistributing function memory

--- deployment/functions.tf
+++ deployment/functions.tf
@@ -2,7 +2,7 @@ resource "google_cloudfunctions_function" "carto_api" {
   name    = "${var.app_name}-api"
   runtime = "python39"
 
-  available_memory_mb   = 512
+  available_memory_mb   = 1024
   source_archive_bucket = google_storage_bucket.code_archives.name
   source_archive_object = google_storage_bucket_object.api_archive.name
   trigger_http          = true
@@ -18,7 +18,7 @@ resource "google_cloudfunctions_function" "carto_client" {
   name    = "${var.app_name}-client"
   runtime = "nodejs14"
 
-  available_memory_mb   = 256
+  available_memory_mb   = 128
   source_archive_bucket = google_storage_bucket.code_archives.name
   source_archive_object = google_storage_bucket_object.client_archive.name
   trigger_http          = true
