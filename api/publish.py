95148ef0c55af0f3aae1c03e9bd2d66ddd59946a
Miles Bardon <milesbardon@gmail.com>
2021-05-23 22:20:33 +0100

    Patching with JSON instead

--- api/publish.py
+++ api/publish.py
@@ -1,14 +1,10 @@
 from discord import Embed
-from flask import current_app
 import requests
 
 
 def publish(token: str, application_id: str, embed: Embed):
-    current_app.logger.debug(f'ApplicationId: {application_id}')
-    current_app.logger.debug(f'Token: {token}')
-
     requests.patch(f'https://discord.com/api/v9/webhooks/{application_id}/{token}/messages/@original',
                    headers={'Content-Type': 'application/json'},
-                   data={'embeds': [embed.to_dict()]})
+                   json={'embeds': [embed.to_dict()]})
 
     return '', 204
