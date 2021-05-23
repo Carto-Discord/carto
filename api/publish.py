3a73f106c5272faea6a8599e39462c008e545abd
Miles Bardon <milesbardon@gmail.com>
2021-05-23 22:34:42 +0100

    Debugging image

--- api/publish.py
+++ api/publish.py
@@ -1,8 +1,11 @@
 from discord import Embed
+from flask import current_app
 import requests
 
 
 def publish(token: str, application_id: str, embed: Embed):
+    current_app.logger.debug(embed.to_dict())
+
     requests.patch(f'https://discord.com/api/v9/webhooks/{application_id}/{token}/messages/@original',
                    headers={'Content-Type': 'application/json'},
                    json={'embeds': [embed.to_dict()]})
