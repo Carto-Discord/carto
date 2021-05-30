3d5f1d92f9a658bc82f241cf9f269334c8f95de4
Miles Bardon <milesbardon@gmail.com>
2021-05-30 22:27:54 +0100

    Adding Cloud Run configuration and removing GCE

--- deployment/iam.tf
+++ deployment/iam.tf
@@ -7,6 +7,14 @@ resource "google_cloudfunctions_function_iam_member" "client_invoker" {
   member = "allUsers"
 }
 
+resource "google_cloud_run_service_iam_member" "api_invoker" {
+  location = google_cloud_run_service.api.location
+  project = google_cloud_run_service.api.project
+  service = google_cloud_run_service.api.name
+  role = "roles/run.invoker"
+  member = "allUsers"
+}
+
 resource "google_storage_bucket_iam_member" "public_rule" {
   bucket = google_storage_bucket.map_storage.name
   role   = "roles/storage.objectViewer"
---
+++ deployment/run.tf
@@ -0,0 +1,27 @@
+resource "google_cloud_run_service" "api" {
+  name     = "${var.app_name}-api"
+  location = var.function_location
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/${var.app_name}/api"
+
+        ports {
+          name = "gunicorn"
+          container_port = 8080
+        }
+
+        env {
+          name = "MAP_BUCKET"
+          value = google_storage_bucket.map_storage.name
+        }
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
