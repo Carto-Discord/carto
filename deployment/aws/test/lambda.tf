7c7d4aae5a3ce5cc81e9496e1d86e762695e1e12
Miles Bardon <milesbardon@gmail.com>
2021-10-16 17:57:54 +0100

    Formatting Client correctly and Lambda deployment scripts

---
+++ deployment/aws/test/lambda.tf
@@ -0,0 +1,48 @@
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+data "archive_file" "client_zip" {
+  type             = "zip"
+  source_dir       = "${path.module}/../../../client/package/"
+  output_file_mode = "0666"
+  output_path      = "${path.module}/files/client.zip"
+}
+
+resource "aws_lambda_function" "client_lambda" {
+  filename      = data.archive_file.client_zip.output_path
+  function_name = "${var.app_name}-client"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "build/src/index.slashFunction"
+
+  source_code_hash = filebase64sha256(data.archive_file.client_zip.output_path)
+
+  runtime = "nodejs14.x"
+
+  environment {
+    variables = {
+      PUBLIC_KEY = var.discord_public_key
+    }
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_logs,
+    aws_cloudwatch_log_group.client-lambda,
+  ]
+}
---
+++ deployment/aws/test/variables.tf
@@ -0,0 +1,7 @@
+variable "app_name" {
+  default = "carto-bot"
+}
+
+variable "discord_public_key" {
+  description = "Discord App Public Key"
+}
