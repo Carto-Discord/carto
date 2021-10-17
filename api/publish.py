38f9695071809cb31668be4f309beef049f464f2
Miles Bardon <milesbardon@gmail.com>
2021-10-17 10:57:05 +0100

    Adding DRY_RUN flag to API for testing@

--- api/publish.py
+++ api/publish.py
@@ -1,3 +1,5 @@
+import os
+
 from discord import Embed
 from flask import current_app
 import requests
@@ -5,6 +7,10 @@ import requests
 
 def publish(token: str, application_id: str, embed: Embed):
     current_app.logger.debug(embed.to_dict())
+    is_test = os.getenv('DRY_RUN') == 'true'
+
+    # Dry runs should not call the Discord API
+    if is_test: return '', 204
 
     requests.patch(f'https://discord.com/api/v9/webhooks/{application_id}/{token}/messages/@original',
                    headers={'Content-Type': 'application/json'},
