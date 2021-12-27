170498fbde8f96e39e630f7a18bb5052e6ddcbbd
Miles Bardon <milesbardon@gmail.com>
2021-12-27 20:52:59 +0000

    Adding register-commands package

---
+++ packages/register-commands/src/types.ts
@@ -0,0 +1,13 @@
+export enum CommandGroup {
+  MAP = "map",
+  TOKEN = "token",
+}
+
+export enum SubCommand {
+  MAP_CREATE = "create",
+  MAP_GET = "get",
+  MAP_DELETE = "delete",
+  TOKEN_ADD = "add",
+  TOKEN_MOVE = "move",
+  TOKEN_DELETE = "delete",
+}
