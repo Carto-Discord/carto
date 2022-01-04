59038bd721536059d4f3aed8072a4fd568ef0021
Miles Bardon <milesbardon@gmail.com>
2022-01-04 16:28:38 +0000

    [debug] Logging in validate command

--- packages/parse-command/src/validation.ts
+++ packages/parse-command/src/validation.ts
@@ -2,10 +2,14 @@ import { APIGatewayProxyEvent } from "aws-lambda";
 import nacl from "tweetnacl";
 
 export const validateRequest = (event: APIGatewayProxyEvent) => {
+  console.log("Validate: ", event);
+
   const publicKey = process.env.PUBLIC_KEY;
   const signature = event.headers["x-signature-ed25519"];
   const timestamp = event.headers["x-signature-timestamp"];
   const { body } = event;
+
+  console.log({ publicKey, signature, timestamp, body });
 
   if (!publicKey || !signature || !timestamp || !body) return false;
 
