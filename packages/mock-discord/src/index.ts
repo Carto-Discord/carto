24570c453a052454d7370bbaf1e4b88183e426a4
Miles Bardon <milesbardon@gmail.com>
2022-01-04 10:32:31 +0000

    Adding Mock Discord client package

---
+++ packages/mock-discord/src/index.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+import http from "http";
+import WebSocket from "ws";
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const port = 3000;
+
+app.use(express.json());
+app.use(
+  express.static(
+    path.join(dirname(fileURLToPath(import.meta.url)), "/../public")
+  )
+);
+
+app.patch("/webhooks/:applicationId/:token/messages/@original", (req, res) => {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(req.params));
+      client.send(JSON.stringify(req.body));
+    }
+  });
+  res.sendStatus(204);
+});
+
+server.listen(port, () => {
+  console.log(`Discord Mock listening at http://localhost:${port}`);
+});
