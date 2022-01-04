0f1c883cd285d015bb10b4566370891df1a71d55
Miles Bardon <milesbardon@gmail.com>
2022-01-04 16:05:01 +0000

    [debug] Uploading tfstate

--- packages/parse-command/tests/validation.test.ts
+++ packages/parse-command/tests/validation.test.ts
@@ -1,40 +1,26 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
-import nacl from "tweetnacl";
 import { validateRequest } from "../src/validation";
 
-jest.mock("tweetnacl");
-
-const mockVerify = nacl.sign.detached.verify as jest.MockedFunction<
-  typeof nacl.sign.detached.verify
->;
-
 describe("Validation", () => {
-  mockVerify.mockReturnValue(true);
-
   beforeEach(() => {
-    process.env.PUBLIC_KEY = "public key";
+    process.env.PUBLIC_KEY =
+      "a58293d7fa04d53ab90108be275a87fe890eece595962273a93e1f87d398df74";
   });
 
   it("should verify the request", () => {
-    const body = JSON.stringify({
-      data: true,
-    });
+    const body = JSON.stringify({ type: 1 });
 
     const mockEvent = {
       headers: {
-        "x-signature-ed25519": "signature",
-        "x-signature-timestamp": "timestamp",
+        "x-signature-ed25519":
+          "3a25f3db58760e73b1a0ac5163b331e23c81483177d819158a6c6514a54a3a0ad630fdb35b0bd110d6b701511f70cb28fa00fd417e8868341ce0f25d488b390b",
+        "x-signature-timestamp": "1641309313810",
       },
       body,
     } as unknown as APIGatewayProxyEvent;
 
     const result = validateRequest(mockEvent);
 
-    expect(mockVerify).toBeCalledWith(
-      Buffer.from("timestamp" + body),
-      Buffer.from("signature", "hex"),
-      Buffer.from("public key", "hex")
-    );
     expect(result).toBe(true);
   });
 });
