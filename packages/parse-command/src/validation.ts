80148ac465b5b1dad1109be68079632ef6d657da
Miles Bardon <milesbardon@gmail.com>
2022-01-04 16:48:39 +0000

    Fixing validation header parsing

--- packages/parse-command/src/validation.ts
+++ packages/parse-command/src/validation.ts
@@ -1,13 +1,22 @@
-import { APIGatewayProxyEvent } from "aws-lambda";
+import { APIGatewayProxyEvent, APIGatewayProxyEventHeaders } from "aws-lambda";
 import nacl from "tweetnacl";
+
+const getCaseInsensitive = (
+  headers: APIGatewayProxyEventHeaders,
+  key: string
+) => {
+  const correspondingKey = Object.keys(headers).find(
+    (k) => k.toLowerCase() === key.toLowerCase()
+  );
+
+  return correspondingKey ? headers[correspondingKey] : undefined;
+};
 
 export const validateRequest = (event: APIGatewayProxyEvent) => {
   const publicKey = process.env.PUBLIC_KEY;
-  const signature = event.headers["x-signature-ed25519"];
-  const timestamp = event.headers["x-signature-timestamp"];
+  const signature = getCaseInsensitive(event.headers, "x-signature-ed25519");
+  const timestamp = getCaseInsensitive(event.headers, "x-signature-timestamp");
   const { body } = event;
-
-  console.log({ publicKey, signature, timestamp, body });
 
   if (!publicKey || !signature || !timestamp || !body) return false;
 
--- packages/parse-command/tests/validation.test.ts
+++ packages/parse-command/tests/validation.test.ts
@@ -12,9 +12,9 @@ describe("Validation", () => {
 
     const mockEvent = {
       headers: {
-        "x-signature-ed25519":
+        "X-Signature-Ed25519":
           "3a25f3db58760e73b1a0ac5163b331e23c81483177d819158a6c6514a54a3a0ad630fdb35b0bd110d6b701511f70cb28fa00fd417e8868341ce0f25d488b390b",
-        "x-signature-timestamp": "1641309313810",
+        "X-Signature-Timestamp": "1641309313810",
       },
       body,
     } as unknown as APIGatewayProxyEvent;
