e5db7104af3572deadfc7136a763f893667c7e57
Miles Bardon <milesbardon@gmail.com>
2022-01-07 10:07:36 +0000

    Renaming retrieve-map to get-map

--- packages/terraform/localstack/lambda.tf
+++ packages/terraform/localstack/lambda.tf
@@ -36,7 +36,7 @@ module "get_map_lambda" {
   source = "../lambda"
   
   app_name = var.app_name
-  function_name = "retrieve-map"
+  function_name = "get-map"
   runtime = "nodejs14.x"
   lambda_iam_role_arn = aws_iam_role.iam_for_lambda.arn
   lambda_iam_role_name = aws_iam_role.iam_for_lambda.name
