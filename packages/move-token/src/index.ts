6fbd512747ffdd6ba5bdaec455dfcc95cf9fd284
Miles Bardon <milesbardon@gmail.com>
2022-01-13 22:05:10 +0000

    Adding move-token package

---
+++ packages/move-token/src/index.ts
@@ -0,0 +1,249 @@
+import type { APIGatewayProxyResult } from "aws-lambda";
+import {
+  applyTokensToGrid,
+  downloadImage,
+  validateMapData,
+  validateTokenPosition,
+  Size,
+  Token,
+  uploadMap,
+} from "@carto/token-utils";
+import { nanoid } from "nanoid";
+
+type Event = {
+  application_id: string;
+  channel_id: string;
+  token: string;
+  name: string;
+  row: number;
+  column: string;
+};
+
+const ERROR_TITLE = "Token Move error";
+const SUCCESS_TITLE = "Token moved";
+
+// Hack to make Canvas work on Lambda
+if (process.env["LAMBDA_TASK_ROOT"]) {
+  process.env["PATH"] =
+    process.env["PATH"] + ":" + process.env["LAMBDA_TASK_ROOT"] + "/lib";
+  process.env["LD_LIBRARY_PATH"] = process.env["LAMBDA_TASK_ROOT"] + "/lib";
+  process.env["PKG_CONFIG_PATH"] = process.env["LAMBDA_TASK_ROOT"] + "/lib";
+}
+
+export const handler = async ({
+  application_id,
+  channel_id,
+  token,
+  name,
+  row,
+  column,
+}: Event): Promise<APIGatewayProxyResult> => {
+  const serverError = {
+    statusCode: 500,
+    body: JSON.stringify({
+      application_id,
+      token,
+      embed: {
+        title: ERROR_TITLE,
+        description:
+          "Map data for this channel is incomplete.\nCreate the map again or [report it](https://www.github.com/carto-discord/carto/issues).",
+        type: "rich",
+      },
+    }),
+  };
+
+  const validation = await validateMapData(channel_id);
+
+  if (validation.statusCode !== 200) {
+    if (validation.statusCode === 404) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          application_id,
+          token,
+          embed: {
+            title: ERROR_TITLE,
+            description:
+              "No map exists for this channel.\nCreate one with the `/map create` command",
+            type: "rich",
+          },
+        }),
+      };
+    }
+
+    return serverError;
+  }
+
+  const { baseMapData, baseMapFilename, currentMapData } = validation;
+
+  if (!baseMapData.Item || !currentMapData.Item) return serverError;
+
+  const { margin, rows, columns } = baseMapData.Item;
+  const { tokens } = currentMapData.Item;
+
+  if (!margin?.M || !rows?.N || !columns?.N || !tokens?.L) return serverError;
+
+  const tokenNames = tokens.L.map(({ M }) => M?.name?.S);
+
+  if (!tokenNames.includes(name)) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        application_id,
+        token,
+        embed: {
+          title: ERROR_TITLE,
+          description: `Token ${name} not found in map. Token names are case sensitive, so try again or add it using \`/token add\``,
+          type: "rich",
+        },
+      }),
+    };
+  }
+
+  const tokenPositionValid = validateTokenPosition({
+    token: { row, column },
+    grid: { rows: parseInt(rows.N), columns: parseInt(columns.N) },
+  });
+
+  if (!tokenPositionValid) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        application_id,
+        token,
+        embed: {
+          title: ERROR_TITLE,
+          description: `The row or column you entered is out of bounds.\nThis map's bounds are ${rows.N} rows by ${columns.N} columns`,
+          type: "rich",
+        },
+      }),
+    };
+  }
+
+  // Add new token onto existing tokens
+  const newTokens: Token[] = tokens.L.map((token) => {
+    const {
+      name: tokenName,
+      row: tokenRow,
+      column: tokenColumn,
+      color,
+      size,
+    } = token.M || {};
+
+    const tokenColor = color.S || "";
+    const tokenSize = parseFloat(size.N || Size.MEDIUM.toString());
+
+    if (tokenName.S === name) {
+      return {
+        name,
+        row,
+        column,
+        color: tokenColor,
+        size: tokenSize,
+      };
+    }
+
+    return {
+      name: tokenName.S || "",
+      row: parseInt(tokenRow.N || "0"),
+      column: tokenColumn.S || "",
+      color: tokenColor,
+      size: tokenSize,
+    };
+  });
+
+  const baseFilename = `${
+    process.env["LAMBDA_TASK_ROOT"] ? "/tmp/" : ""
+  }download.png`;
+
+  try {
+    await downloadImage(baseMapFilename, baseFilename);
+  } catch (error) {
+    console.warn(error);
+    return {
+      statusCode: 404,
+      body: JSON.stringify({
+        application_id,
+        token,
+        embed: {
+          title: ERROR_TITLE,
+          description:
+            "Map could not be recreated. Reason: Original map could not be found",
+          type: "rich",
+        },
+      }),
+    };
+  }
+
+  const mapId = nanoid();
+
+  const mapBuffer = await applyTokensToGrid({
+    baseFilename,
+    margin: {
+      x: parseFloat(margin.M?.x?.N || "0.0"),
+      y: parseFloat(margin.M?.y?.N || "0.0"),
+    },
+    tokens: newTokens,
+  });
+
+  if (!mapBuffer) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        application_id,
+        token,
+        embed: {
+          title: ERROR_TITLE,
+          description: "Map could not be created",
+          type: "rich",
+        },
+      }),
+    };
+  }
+
+  const successful = await uploadMap({
+    buffer: mapBuffer,
+    channelId: channel_id,
+    mapId,
+    tokens: newTokens,
+  });
+
+  if (successful) {
+    const imageUrl = `https://s3.${process.env.AWS_REGION}.amazonaws.com/${process.env.MAPS_BUCKET}/${mapId}.png`;
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        application_id,
+        token,
+        embed: {
+          title: SUCCESS_TITLE,
+          description: "Token positions:",
+          image: {
+            url: imageUrl,
+          },
+          fields: newTokens.map((token) => ({
+            name: token.name,
+            value: `${token.column.toUpperCase()}${token.row}`,
+            inline: true,
+          })),
+          type: "rich",
+        },
+      }),
+    };
+  } else {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        application_id,
+        token,
+        embed: {
+          title: ERROR_TITLE,
+          description:
+            "An error occured while creating the new map. Please try again later",
+          type: "rich",
+        },
+      }),
+    };
+  }
+};
---
+++ packages/move-token/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.ts",
+  resolve: {
+    extensions: [".js", ".json", ".ts"],
+    alias: {
+      "@carto/token-utils": path.resolve(__dirname, "../token-utils"),
+    },
+  },
+  output: {
+    libraryTarget: "commonjs",
+    path: path.join(__dirname, "dist"),
+    filename: "index.js",
+  },
+  target: "node",
+  optimization: {
+    usedExports: true,
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        // Include ts files.
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "swc-loader",
+        },
+      },
+      {
+        test: /\.node$/,
+        use: "node-loader",
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "../../assets/fonts", to: "./" }],
+    }),
+  ],
+};
