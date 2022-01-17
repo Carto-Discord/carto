586ed0f9063bb53397a592a98b56a0c84fd35131
Miles Bardon <milesbardon@gmail.com>
2022-01-17 19:44:40 +0000

    Adding error handling capability to send-response function

--- packages/send-response/src/index.ts
+++ packages/send-response/src/index.ts
@@ -1,20 +1,30 @@
 import axios from "axios";
 import type { MessageEmbed } from "discord.js";
 
-type Event = {
+export type Event = {
   application_id: string;
   token: string;
-  embed: MessageEmbed;
+  embed?: MessageEmbed;
+  error?: string;
 };
 
 export const handler = async (event: string) => {
-  const { application_id, token, embed }: Event = JSON.parse(event);
+  const { application_id, token, embed, error }: Event = JSON.parse(event);
 
   const url = `${process.env.BASE_URL}/webhooks/${application_id}/${token}/messages/@original`;
 
-  await axios.patch(
+  const errorEmbed = {
+    title: "Command execution failed",
+    description:
+      "The command failed to process, likely due to an internal error.",
+    type: "rich",
+  };
+
+  if (error) console.warn({ error }, "Handling error");
+
+  return axios.patch(
     url,
-    { embeds: [embed] },
+    { embeds: [error ? errorEmbed : embed] },
     { headers: { "Content-Type": "application/json" } }
   );
 };
