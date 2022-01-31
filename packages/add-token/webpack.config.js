db2bb279a60e7e543b53c91330f5cd3bc1892701
Miles Bardon <milesbardon@gmail.com>
2022-01-31 22:26:30 +0000

    Configuring add-token to use project references

--- packages/add-token/webpack.config.js
+++ packages/add-token/webpack.config.js
@@ -1,9 +1,15 @@
 const path = require("path");
 const { merge } = require("webpack-merge");
 const CopyPlugin = require("copy-webpack-plugin");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
 const common = require("../../webpack.common.js");
 
 module.exports = merge(common, {
+  resolve: {
+    extensions: [".js", ".json", ".ts"],
+    plugins: [new TsconfigPathsPlugin()],
+  },
   output: {
     path: path.join(__dirname, "dist"),
   },
