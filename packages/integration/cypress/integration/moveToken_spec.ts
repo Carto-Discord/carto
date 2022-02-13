64d984ecb10ba2bdc663bc0b04ea32d830f421db
Miles Bardon <milesbardon@gmail.com>
2022-02-13 13:22:38 +0000

    Fixing linting issues in other integration tests

--- packages/integration/cypress/integration/moveToken_spec.ts
+++ packages/integration/cypress/integration/moveToken_spec.ts
@@ -137,7 +137,7 @@ describe("Move Token", () => {
           token,
         });
         const embed = body.embeds[0];
-        newImageId = embed.image.url.replace(/^.*[\\\/]/, "").split(".")[0];
+        newImageId = embed.image.url.replace(/^.*[\\/]/, "").split(".")[0];
 
         expect(embed.image.url).to.eq(
           `https://s3.us-east-1.amazonaws.com/carto-bot-maps/${newImageId}.png`
