19bc94cb881c2c25c45362e2ba1dcbbb19c28c97
Miles Bardon <milesbardon@gmail.com>
2022-02-23 08:39:18 +0000

    Changing tables to be pay-per-request

--- packages/terraform/localstack/dynamodb.tf
+++ packages/terraform/localstack/dynamodb.tf
@@ -1,9 +1,7 @@
 resource "aws_dynamodb_table" "map_table" {
-  name           = "${var.app_name}-maps"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 20
-  write_capacity = 20
-  hash_key       = "id"
+  name         = "${var.app_name}-maps"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
 
   attribute {
     name = "id"
@@ -12,11 +10,9 @@ resource "aws_dynamodb_table" "map_table" {
 }
 
 resource "aws_dynamodb_table" "channel_table" {
-  name           = "${var.app_name}-channels"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 20
-  write_capacity = 20
-  hash_key       = "id"
+  name         = "${var.app_name}-channels"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
 
   attribute {
     name = "id"
