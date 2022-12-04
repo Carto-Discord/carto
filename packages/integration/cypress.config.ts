8e16692bdd12907a8a7519abd081ed7c5ce8459d
Miles Bardon <milesbardon@gmail.com>
2022-12-04 15:52:21 +0000

    Fixing buffer undefined issue

--- packages/integration/cypress.config.ts
+++ packages/integration/cypress.config.ts
@@ -1,6 +1,8 @@
 import path from "path";
 
 import { defineConfig } from "cypress";
+import { ProvidePlugin } from "webpack";
+
 import webpackPreprocessor from "@cypress/webpack-preprocessor";
 
 export default defineConfig({
@@ -40,11 +42,19 @@ export default defineConfig({
             },
             resolve: {
               extensions: [".ts", ".js"],
+              fallback: {
+                buffer: require.resolve("buffer"),
+              },
             },
             output: {
               filename: "bundle.js",
               path: path.resolve(__dirname, "dist"),
             },
+            plugins: [
+              new ProvidePlugin({
+                Buffer: ["buffer", "Buffer"],
+              }),
+            ],
           },
         })
       );
