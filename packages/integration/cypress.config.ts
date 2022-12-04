2d16d667a37dca9163e52041dbf162be5e66f4f4
Miles Bardon <milesbardon@gmail.com>
2022-12-04 15:38:42 +0000

    Adding custom webpack config for Cypress

--- packages/integration/cypress.config.ts
+++ packages/integration/cypress.config.ts
@@ -1,4 +1,7 @@
-import { defineConfig } from 'cypress'
+import path from "path";
+
+import { defineConfig } from "cypress";
+import webpackPreprocessor from "@cypress/webpack-preprocessor";
 
 export default defineConfig({
   video: false,
@@ -8,7 +11,44 @@ export default defineConfig({
     openMode: 0,
   },
   e2e: {
-    setupNodeEvents(on, config) {},
-    baseUrl: 'http://localhost:3000',
+    setupNodeEvents(on) {
+      on(
+        "file:preprocessor",
+        webpackPreprocessor({
+          webpackOptions: {
+            mode: "development",
+            module: {
+              rules: [
+                {
+                  test: /\.js$/,
+                  exclude: [/node_modules/],
+                  use: [
+                    {
+                      loader: "babel-loader",
+                      options: {
+                        presets: ["@babel/preset-env"],
+                      },
+                    },
+                  ],
+                },
+                {
+                  test: /\.ts$/,
+                  exclude: [/node_modules/],
+                  use: "ts-loader",
+                },
+              ],
+            },
+            resolve: {
+              extensions: [".ts", ".js"],
+            },
+            output: {
+              filename: "bundle.js",
+              path: path.resolve(__dirname, "dist"),
+            },
+          },
+        })
+      );
+    },
+    baseUrl: "http://localhost:3000",
   },
-})
+});
